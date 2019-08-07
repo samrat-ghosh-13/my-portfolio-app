e33ddc0bab6ff63fc470d7f866598e3c8f9f08cc
samghosh <samghosh@deloitte.com>
2019-08-08 01:58:32 +0530

    Changes to Education and Skill Modules
    Changes to Education and Skill Modules

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faJsfiddle,
+  faNode,
+  faAngular,
+  faReact,
+  faVuejs,
+  faNpm,
+  faGitAlt,
+  faGithub,
+  faBootstrap,
+  faJira,
+  faFreeCodeCamp,
+  faBitbucket
+} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  selector: "app-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.css"]
 })
 export class SkillsComponent implements OnInit {
+  faHtml5 = faHtml5;
+  faCss3 = faCss3Alt;
+  faJs = faJs;
+  faJsfiddle = faJsfiddle;
+  faNodeJs = faNode;
+  faAngular = faAngular;
+  faReact = faReact;
+  faVuejs = faVuejs;
+  faNpm = faNpm;
+  faGitAlt = faGitAlt;
+  faGithub = faGithub;
+  faBootstrap = faBootstrap;
+  faJira = faJira;
+  faFreeCodeCamp = faFreeCodeCamp;
+  faBitbucket = faBitbucket;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
