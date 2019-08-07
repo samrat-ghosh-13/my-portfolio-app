9c4ffe1f25be5446eb91e97eb8b375bd8922426f
samghosh <samghosh@deloitte.com>
2019-08-07 18:24:05 +0530

    About Page Updates Commit
    About Page Updates Commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  faFacebookF,
+  faTwitter,
+  faLinkedin,
+  faGithub, 
+  faMediumM
+} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
+  faFacebookF = faFacebookF;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faMedium = faMediumM;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { AboutComponent } from "./about/about.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavigationComponent, AboutComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    library.add(faCoffee);
+  }
+}
