25915d90997cc1a95d0aa71c6f67ea73396ef39d
samghosh <samghosh@deloitte.com>
2019-08-07 23:17:00 +0530

    Experience and About Component
    Experience and About Component working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,73 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavigationComponent } from "./navigation/navigation.component";
 import { AboutComponent } from "./about/about.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { Routes, RouterModule } from "@angular/router";
+import { EducationComponent } from "./education/education.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { InterestsComponent } from "./interests/interests.component";
+import { AwardsComponent } from "./awards/awards.component";
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: AboutComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "experience",
+    component: ExperienceComponent
+  },
+  {
+    path: "education",
+    component: EducationComponent
+  },
+  {
+    path: "skills",
+    component: SkillsComponent
+  },
+  {
+    path: "interests",
+    component: InterestsComponent
+  },
+  {
+    path: "awards",
+    component: AwardsComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestsComponent,
+    AwardsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(){
+  constructor() {
     library.add(faCoffee);
   }
 }
